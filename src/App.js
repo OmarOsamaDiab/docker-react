ae246e1fffc053b48419b3f9360afaad38b7cad7
omar osama <omarosamadiab10@gmail.com>
2021-02-27 17:49:18 +0200

    changed the text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          omar osama Ali
+          my name is omar osama ali mohamed
         </p>
         <a
           className="App-link"
